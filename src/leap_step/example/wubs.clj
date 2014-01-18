ec3b1e37a3d27299e751164ff94a45642c4d5430
vikas <skvikas@umich.edu>
2014-01-18 15:06:04 -0500

    Added one-handed, two-handed and fist controls for dubstep. one-handed = low. two-handed = mid-range fifths. fist = high pitch chords

--- src/leap_step/example/wubs.clj
+++ src/leap_step/example/wubs.clj
@@ -5,22 +5,72 @@
             [leap-step.example.popular :as popular]))
 
 (defn process-frame [frame dubs]
-  (do      (if (leap/single-hand? frame)
+  (do      (cond
+            (not (leap/hands? frame)) "false"
+            (leap/single-hand? frame)
              (let [low-hand (leap/lowest-hand frame)
                    pos (hand/palm-position low-hand)
                    x (.getX pos)
                    y (.getY pos)
                    z (.getZ pos)]
                (println pos)
-               (when true ;(< y 150)
-                (condp > x
-                  -300 "No Zone"
-                  -150 (live/ctl dubs :note 34)
-                  0 (live/ctl dubs :note 37)
-                  150 (live/ctl dubs :note 39)
-                  300 (live/ctl dubs :note 42)
-                  "No Zone")))
-             "false")))
+               (when (< z 150)
+                (condp > y
+                  ; "No Zone"
+                  100 (live/ctl dubs :wobble 1)
+                  175 (live/ctl dubs :wobble 2)
+                  250 (live/ctl dubs :wobble 4)
+                  325 (live/ctl dubs :wobble 8)
+                  400 (live/ctl dubs :wobble 12)
+                  475 (live/ctl dubs :wobble 16)
+                  "No Zone")
+
+                (if-not (hand/fist? low-hand)
+                  (condp > x
+                    -300 "No Zone"
+                    -150 (live/ctl dubs :note 34 :chord-vol 0 :wob4-vol 0 :wob-vol 1)
+                    0 (live/ctl dubs :note 37 :chord-vol 0 :wob4-vol 0 :wob-vol 1)
+                    150 (live/ctl dubs :note 39 :chord-vol 0 :wob4-vol 0 :wob-vol 1)
+                    300 (live/ctl dubs :note 42 :chord-vol 0 :wob4-vol 0 :wob-vol 1)
+                    "No Zone")
+                  (condp > x
+                    -300 "No Zone"
+                    -125 (live/ctl dubs :note 58 :chord-vol 1 :wob4-vol 0)
+                    0 (live/ctl dubs :note 61 :chord-vol 1 :wob4-vol 0)
+                    150 (live/ctl dubs :note 63 :chord-vol 1 :wob4-vol 0)
+                    300 (live/ctl dubs :note 68 :chord-vol 1 :wob4-vol 0)
+                    "No Zone"))))
+             :else    
+             (let [lefthand (leap/leftmost-hand frame)
+                   pos1 (hand/palm-position lefthand)
+                   x1 (.getX pos1)
+                   y1 (.getY pos1)
+                   z1 (.getZ pos1)
+                   righthand (leap/rightmost-hand frame)
+                   pos2 (hand/palm-position righthand)
+                   x2 (.getX pos2)
+                   y2 (.getY pos2)
+                   z2 (.getZ pos2)]
+                   (when (and (< z1 150) (< z2 150))
+                    (println (- x1 x2))
+                    (condp > y1
+                      ; "No Zone"
+                      100 (live/ctl dubs :wobble 1)
+                      175 (live/ctl dubs :wobble 2)
+                      250 (live/ctl dubs :wobble 4)
+                      325 (live/ctl dubs :wobble 8)
+                      400 (live/ctl dubs :wobble 12)
+                      475 (live/ctl dubs :wobble 16)
+                      "No Zone")
+
+                    
+                    (condp > (- x1 x2)
+                      -700 "No Zone"
+                      -400 (live/ctl dubs :note 34 :chord-vol 0 :wob4-vol 1 :wob-vol 1.5)
+                      -300 (live/ctl dubs :note 37 :chord-vol 0 :wob4-vol 1 :wob-vol 1.5)
+                      -150 (live/ctl dubs :note 39 :chord-vol 0 :wob4-vol 1 :wob-vol 1.5)
+                      -80 (live/ctl dubs :note 42 :chord-vol 0 :wob4-vol 1 :wob-vol 1.5)
+                      "No Zone"))))))
 
   ;(cond
     ;(not (leap/hands? frame)) (do (live/ctl dubs :note 50 :wobble 1))
