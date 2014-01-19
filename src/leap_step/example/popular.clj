20e2bee7d9644b93f82fe268c3d10780cfc75d5f
Charles Lewis <noodle@umich.edu>
2014-01-19 00:52:13 -0500

    Wub function - complete!

--- src/leap_step/example/popular.clj
+++ src/leap_step/example/popular.clj
@@ -1,62 +1,29 @@
 (ns leap-step.example.popular
   (:use [overtone.core]))
 
+
 (defsynth dubstep [rootNote 34 bpm 120 wobble 8 note 34 snare-vol 1 kick-vol 1 hihat-vol 1 v 1 chord-vol 0 wob-vol 0 wob4-vol 0 out-bus 0]
- (let [trig (impulse:kr (/ bpm 120))
-       freq (midicps note)
 
-       cf1 (midicps (- note 5))
-       cf2 (midicps (+ note 2))
-       cf3 (midicps (- note 24))
-
-       freq4 (midicps (+ note 19))
-       freq5 (midicps (+ note 12))
-       freq6 (midicps (+ note 7))
-
-       swr (demand trig 0 (dseq [wobble] INF))
-       sweep (lin-exp (lf-tri swr) -1 1 40 3000)
-       wob (apply + (saw (* freq [0.99 1.01])))
-       wob (lpf wob sweep)
-       wob (* (+ (* wobble 0.02) 0.4) (normalizer wob))
-       wob (+ wob (bpf wob 9000 2))
-       wob (+ wob (* 0.3 (g-verb wob 8 0.7 0.7)))
+  (defn wub [offset b-freq roomsize]
+    (let [trig-i (impulse:kr (/ bpm 120))
+          freq (midicps (+ note offset))
+          swr (demand trig-i 0 (dseq [wobble] INF))
+          sweep (lin-exp (lf-tri swr) -1 1 40 3000)
+          wob (apply + (saw (* freq [0.99 1.01])))
+          wob (lpf wob sweep)
+          wob (* (+ (* wobble 0.02) 0.4) (normalizer wob))
+          wob (+ wob (bpf wob b-freq 2))
+          wob (+ wob (* 0.3 (g-verb wob roomsize 0.7 0.7)))]
+      wob))
+        
+ (let [wob (wub 0 9000 8)
        ; 2,3,4,6,8,9, (10) 12,14,16
-       crd1 (apply + (saw (* cf1 [0.99 1.01])))
-       crd1 (lpf crd1 sweep)
-       crd1 (* (+ (* wobble 0.02) 0.4) (normalizer crd1))
-       crd1 (+ crd1 (bpf crd1 2000 2))
-       crd1 (+ crd1 (* 0.3 (g-verb crd1 13 0.7 0.7)))
-
-       crd2 (apply + (saw (* cf2 [0.99 1.01])))
-       crd2 (lpf crd2 sweep)
-       crd2 (* (+ (* wobble 0.02) 0.4) (normalizer crd2))
-       crd2 (+ crd2 (bpf crd2 2000 2))
-       crd2 (+ crd2 (* 0.3 (g-verb crd2 13 0.7 0.7)))
-
-       crd3 (apply + (saw (* cf3 [0.99 1.01])))
-       crd3 (lpf crd3 sweep)
-       crd3 (* (+ (* wobble 0.02) 0.4) (normalizer crd3))
-       crd3 (+ crd3 (bpf crd3 2000 2))
-       crd3 (+ crd3 (* 0.3 (g-verb crd3 13 0.7 0.7)))
-
-       wob4 (apply + (saw (* freq4 [0.99 1.01])))
-       wob4 (lpf wob4 sweep)
-       wob4 (* (+ (* wobble 0.02) 0.4) (normalizer wob4))
-       wob4 (+ wob4 (bpf wob4 2000 2))
-       wob4 (+ wob4 (* 0.3 (g-verb wob4 13 0.7 0.7)))
-
-       wob5 (apply + (saw (* freq5 [0.99 1.01])))
-       wob5 (lpf wob5 sweep)
-       wob5 (* (+ (* wobble 0.02) 0.4) (normalizer wob5))
-       wob5 (+ wob5 (bpf wob5 2000 2))
-       wob5 (+ wob5 (* 0.3 (g-verb wob5 13 0.7 0.7)))
-
-       wob6 (apply + (saw (* freq6 [0.99 1.01])))
-       wob6 (lpf wob6 sweep)
-       wob6 (* (+ (* wobble 0.02) 0.4) (normalizer wob6))
-       wob6 (+ wob6 (bpf wob6 2000 2))
-       wob6 (+ wob6 (* 0.3 (g-verb wob6 13 0.7 0.7)))
-
+       crd1 (wub -5 2000 13)
+       crd2 (wub 2 2000 13)
+       crd3 (wub -24 2000 13)
+       wob4 (wub 19 2000 13)
+       wob5 (wub 12 2000 13)
+       wob6 (wub 7 2000 13)
        kickenv (decay (t2a (demand (impulse:kr (/ bpm 30)) 0 (dseq [1 0 0 1 0 0 1 0 1 0 0 1 0 1 0 0] INF))) 0.7)
        kick (* (* kickenv 7) (sin-osc (+ 20 (* kickenv kickenv kickenv 150))))
        kick (clip2 kick 1)
